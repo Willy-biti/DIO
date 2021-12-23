b0daaeeec96eaf541cc1b5b538aa70ccb5e292dd
Willy Biti <willy.biti@gmail.com>
2021-12-23 13:18:25 -0300

    primeiro commit

---
+++ CURSOS/MySql/intro.sql
@@ -0,0 +1,20 @@
+CREATE TABLE pessoas (                                                                   --Criar TABELA
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,                    
+    nome VARCHAR (30) NOT NULL,
+    nascimento DATE
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Willy', '1997 03 20')                      --INSERT INTO sert a inserer les données
+INSERT INTO pessoas (nome, nascimento) VALUES ('Biti', '1996 03 20')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Emilie', '1965 08 14')
+
+
+SELECT * FROM pessoas      -- SELECIONNER TOUT LE TABLEAU
+
+UPDATE pessoas SET nome = 'Willy Biti';     --ADDICIONER OU ALTERER UN TRUC EXISTANT
+
+SELECT FROM pessoas WHERE id=2;     --SELECTIONNER AVEC PRECISION
+DELETE FROM pessoas WHERE id=2;     --SUPPRIMER AVEC PRECISION
+
+SELECT * FROM pessoas ORDER BY nome  --ORDENAÇÃO CRESCENTE
+SELECT * FROM pessoas ORDER BY nome DESC --ORDENAÇÃO DESCRESECNTE
