7e7b8f35caaaaf2e67b4589bf9c2b6190ffdd30f
Willy Biti <willy.biti@gmail.com>
2021-12-23 15:52:25 -0300

    atualização

--- CURSOS/API/index.ts
+++ CURSOS/API/index.ts
@@ -1,5 +1,31 @@
-function soma(numero: number){
-    return numero + 1
-}
+import express, {Request, Response} from 'express';
 
-console.log(soma(5))
+const server = express();
+
+server.get('/', (request: Request, response: Response)=>{
+    // TOTAS AS FUNCIONALIDADE
+    return response.json({mensagem: 'Bonjour'})
+})
+
+server.listen(5000, () => {
+    console.log('Servidor on na porta 5000 http://localhost:5000/')
+})
+
+
+
+//GET  ler
+//PUT/PACTH  atualizar
+//UPDATE  atualizar
+//DELETE apagar
+
+
+
+// /usuario
+
+
+
+
+//Criar um usário             C
+//Selecinar um usuario    R
+//Editar um usuario         U
+//Deletar um usuiario      D
